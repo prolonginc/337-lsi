9f2a6595fed36a8d549336d2fd2ae47a2156c88b
Daniel Reed <reedtdan@gmail.com>
2017-08-07 09:40:09 -0400

    TDDManagerTest snubs
    Base file

--- source/edu/oakland/test/TDDManagerTest.java
+++ source/edu/oakland/test/TDDManagerTest.java
@@ -4,5 +4,25 @@ import edu.production.*;
 
 
 public class TDDManagerTest {
-
+	private TicTacToeManager manager;
+	private TTTDisplay display;
+	
+	@Before
+	// create display and manager classes
+	public void initializeSystem() {
+		manager = new TicTacToeManager();
+		display = new Display();
+	}
+	
+	@Test
+	public void testPrintCurrentBoard() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testChangePlayer() {
+		display.changePlayer();
+		assertTrue(true);
+	}
+	
 }

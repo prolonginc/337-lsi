08370562139b70c326225f1b4d0863a015b2185a
cWelland96 <cwelland@oakland.edu>
2017-08-07 09:41:08 -0400

    Update with GameBoard class

---
+++ source/edu/oakland/production/GameBoard.java
@@ -0,0 +1,14 @@
+package edu.oakland.helper; 
+
+import edu.oakland.production.*; 
+import java.util.*; 
+
+
+public static void main(String() args){
+// create instance of TTTManager; 
+TicTacToeManager manager = new TicTacToeManager(); 
+// launch and initialize all TTTgame classes; 
+manager.launchApp();
+ 
+manager.chooseSquare(); 
+}

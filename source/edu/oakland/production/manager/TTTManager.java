0e8eac4e4255bd53677453559545818938081303
prolonginc <sariyono@prolongmarketing.com>
2017-08-09 09:21:33 -0400

    fixed the manager class

--- source/edu/oakland/production/manager/TTTManager.java
+++ source/edu/oakland/production/manager/TTTManager.java
@@ -1,7 +1,7 @@
-package edu.oakland.helper; 
+package edu.oakland.helper;
 
-import edu.oakland.production.*; 
-import java.util.*; 
+import edu.oakland.production.*;
+import java.util.*;
 
 public class TTTManager{
 
@@ -12,7 +12,7 @@ private TTTDatabase database;
 private Scanner scanner;
 private char currentPlayer;
 // private char currentPlayerMark;
-private boolean isWinOrDrawResult; 
+private boolean isWinOrDrawResult;
 private boolean gameIsOver;
 //private TTTDatabase database;
 
@@ -22,35 +22,44 @@ private boolean gameIsOver;
 	this.currentPlayer = 'x';
 	}
 public void launchapp() {
-	
+
 // creating new instrances of database, middleware, and display with information revelant to the current turn
-// launch a initialize database; make sure all cells are empty; 
-// launch a intialize middleware; pass in database ref; 
+// launch a initialize database; make sure all cells are empty;
+// launch a intialize middleware; pass in database ref;
 // launch a initialize display; pass in middleware ref;
-	
-	this.database = new TTTDatabase(); 
+
+	this.database = new TTTDatabase();
 	this.middleware = TTTMiddleware(this.database);
 	this.display = TTTDisplay(this.middlware);
-	
+
 
 	// this may need to be its own method
 	while(this.gameIsOver == false){
-	
-	
-	//Displaying the board
-	display.printCurrentBoard();
-	
-	// giving display current team, 
-	display.placeMarkOnBoard(this.currentPlayer);
-	
-	// Checking if game is over
-	this.gameIsOver = display.checkForWinOrDraw();
 
-	// changing the player 
+
+	//Displaying the board
+	this.printCurrentBoard();
+	// giving display current team,
+	this.placeMarkOnBoard();
+	// Checking if game is over
+	this.gameIsOver = this.checkForWinOrDraw();
+
+	// changing the player
 	changePlayer();
 	}
 }
 
+public void placeMarkOnBoard(){
+
+
+	this.display.placeMarkOnBoard(this.currentPlayer)
+}
+
+public boolean checkForWinOrDraw(){
+
+	return display.checkForWinOrDraw();
+
+}
 
 private void changePlayer(){
 	// changing the player
@@ -62,6 +71,12 @@ private void changePlayer(){
 	}
 }
 
+public void printCurrentBoard() {
+	display.printCurrentBoard();
+
+
+}
+
 public char getCurrentPlayer(){
 	return this.currentPlayer;
 }
@@ -70,7 +85,3 @@ public boolean getGameIsOver(){
 	return this.gameIsOver;
 }
 }
-
-
-
-

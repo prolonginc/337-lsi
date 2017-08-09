79f2e9251398fc49f449e8b62a079d815d07c510
prolonginc <sariyono@prolongmarketing.com>
2017-08-09 10:50:51 -0400

    added ttt manager

--- source/edu/oakland/production/GameManager.java
+++ source/edu/oakland/production/GameManager.java
@@ -1,16 +1,18 @@
-package edu.oakland.helper; 
+package edu.oakland.helper;
 
-import edu.oakland.production.*; 
-import java.util.*; 
+import edu.oakland.production.manager.*;
+import java.util.*;
 
 public class GamerManager{
 
 public static void main(String() args){
-// create instance of TTTManager; 
-TicTacToeManager manager = new TicTacToeManager(); 
-// launch and initialize all TTTgame classes; 
+// create instance of TTTManager;
+TTTManager manager = new TTTManager();
+// launch and initialize all TTTgame classes;
 manager.launchApp();
- 
-manager.chooseSquare(); 
+
+manager.chooseSquare();
 }
+
+
 }

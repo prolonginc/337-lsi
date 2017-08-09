7ee2823ca9110fee5c5279240254e8c931dcb773
prolonginc <sariyono@prolongmarketing.com>
2017-08-09 10:53:02 -0400

    changed the main method

--- source/edu/oakland/production/GameManager.java
+++ source/edu/oakland/production/GameManager.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class GamerManager{
 
-public static void main(String() args){
+public static void main(String[] args) {
 // create instance of TTTManager;
 TTTManager manager = new TTTManager();
 // launch and initialize all TTTgame classes;
